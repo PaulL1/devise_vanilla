3da46d8949b58048b0a8bce29098608435ed7fcf
Paul Lambert <paul@planar.id.au>
2014-04-02 20:48:47 +0200

    Working devise - can sign_up, and get to sign_in page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseVanilla::Application.config.secret_key_base = '4b7a733bc2d40bb28b47ab2ec11202d9770cb95e82a045258b8d5bc5194ae3006f8347cb1e2f1f39385375feafc0b9f3a3177d8496b6d6adf848b50cf27b7bfd'
